c5b270febacda285d29365848fb66536fcac64bc
David Im <knighthell@gmail.com>
2023-12-08 14:38:17 +0900

    feat: applied nestjs-cluster library to server-place by env

--- apps/server-place/src/main.ts
+++ apps/server-place/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ServerPlaceModule } from './server-place.module';
+import { Cluster } from '@knighthell-boilerplate-nestjs/nestjs-cluster';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServerPlaceModule);
   await app.listen(3000);
 }
-bootstrap();
+
+if (process.env.CLUSTER_MODE === 'true') {
+  Cluster.start(bootstrap);
+} else {
+  bootstrap();
+}
